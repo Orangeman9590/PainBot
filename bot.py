c41f1fdbd6f6a8109a81f9045c59b684efe6b218
0rang3man <61301004+Orangeman9590@users.noreply.github.com>
2020-08-13 15:27:49 -0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+import os
+client = commands.Bot(command_prefix='?')
+
+
+
+@client.command()
+@commands.has_permissions(administrator=True)
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+    await ctx.channel.send('Loaded Ext')
+@load.error
+async def load_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send('You Do Not Have Permission To Use This Command')
+
+
+
+@client.command()
+@commands.has_permissions(administrator=True)
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    await ctx.channel.send('Unloaded Ext')
+@unload.error
+async def unload_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send('You Do Not Have Permission To Use This Command')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+
+
+client.run('NzMxOTMxNDk1Nzg2MDg2NDIy.XwtOhQ.206W-7HOMQZ_m_uM_QCUCB7RN1I')
+
